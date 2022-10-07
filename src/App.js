017db1a24a1fa4598236f93d30ad82aafc455b9b
nikanoza <nikanozadze1010@gmail.com>
2022-10-07 10:18:29 +0400

    feat: project setup

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className='w-full h-full'>tailwind</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        violet: '#AD1FEA',
+        electric: '#4661E6',
+        darkBlue: '#373F68',
+        lightGray: '#F2F4FF',
+        extraLightGray: '#F7F8FD',
+        darkBlueTwo: '#3A4374',
+        gray: '#647196',
+        carrot: '#F49F85',
+        sky: '#62BCFA',
+        blood: '#D73737',
+        violetHover: '#C75AF6',
+        skyHover: '#7C91F9',
+        boxGray: '#CFD7FF',
+        darkBlueTwoHover: '#656EA3',
+        bloodHover: '#E98888',
+      },
+    },
+  },
+  plugins: [],
+};
