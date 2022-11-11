aeffe4c5139d4ae849e2550b52a2d1d9acd5399a
hilu <nikanozadze1010@gmail.com>
2022-11-11 23:53:29 +0400

    wip: roadmap page

---
+++ src/components/Roadmap/Header.jsx
@@ -0,0 +1,27 @@
+import { Link, useLocation } from 'react-router-dom';
+import { ArrowLeft } from 'svg';
+
+const Header = () => {
+  const location = useLocation();
+  return (
+    <header className='w-full h-24 px-6 flex justify-between items-center bg-darkBlue'>
+      <div>
+        <Link to={'/'} className='flex gap-x-3 items-center'>
+          <ArrowLeft color='#CDD2EE' />
+          <span className='jost-b-h4 text-white'>Go Back</span>
+        </Link>
+        <span className='text-white jost-b-h3'>Roadmap</span>
+      </div>
+      <Link
+        className='px-4 py-3 text-white jost-b-h4 bg-violet hover:bg-violetHover whitespace-nowrap rounded-cardBorderRadius ml-auto'
+        id='add-feedback-btn'
+        to={'/feedbacks/new'}
+        state={{ from: location }}
+      >
+        + Add Feedback
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -1,17 +1,72 @@
-import { useEffect } from 'react';
+import { Header } from 'components/Roadmap';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFeedbacks } from 'store';
 
 const Roadmap = () => {
+  const [filterStatus, setFilterStatus] = useState(2);
+
   const feedbacks = useSelector((state) => state.feedbacks.items);
   const dispatch = useDispatch();
+
+  const planed = feedbacks.filter((feedback) => feedback.status_id === 2);
+  const inProgress = feedbacks.filter((feedback) => feedback.status_id === 3);
+  const live = feedbacks.filter((feedback) => feedback.status_id === 4);
 
   useEffect(() => {
     if (feedbacks.length === 0) {
       dispatch(fetchFeedbacks());
     }
   }, [feedbacks.length, dispatch]);
-  return <div className='w-full h-full bg-extraLightGray'>Roadmap page</div>;
+  return (
+    <div className='w-full h-full bg-extraLightGray'>
+      <Header />
+      <div className='w-full h-16 flex border-b-[#8C92B3] border-b-2 border-opacity-25'>
+        <div
+          className={`w-1/3 flex justify-center items-center border-b-violet ${
+            filterStatus === 2 ? 'border-b-4' : 'border-b-0'
+          }`}
+        >
+          <span
+            className={`text-darkBlueTwo jost-b-h4 ${
+              filterStatus === 2 ? 'opacity-100' : 'opacity-40'
+            }`}
+            onClick={() => setFilterStatus(2)}
+          >
+            Planned ({planed.length})
+          </span>
+        </div>
+        <div
+          className={`w-1/3 flex justify-center items-center border-b-violet ${
+            filterStatus === 3 ? 'border-b-4' : 'border-b-0'
+          }`}
+        >
+          <span
+            className={`text-darkBlueTwo jost-b-h4 ${
+              filterStatus === 3 ? 'opacity-100' : 'opacity-40'
+            }`}
+            onClick={() => setFilterStatus(3)}
+          >
+            In-Progress ({inProgress.length})
+          </span>
+        </div>
+        <div
+          className={`w-1/3 flex justify-center items-center border-b-violet ${
+            filterStatus === 4 ? 'border-b-4' : 'border-b-0'
+          }`}
+        >
+          <span
+            className={`text-darkBlueTwo jost-b-h4 ${
+              filterStatus === 4 ? 'opacity-100' : 'opacity-40'
+            }`}
+            onClick={() => setFilterStatus(4)}
+          >
+            Live ({live.length})
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Roadmap;
